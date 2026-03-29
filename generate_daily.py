45b2ad26230388ed512efde2e8fb671bfe1ee212
Lars Wallenborn <lars@wallenborn.net>
2026-03-29 21:40:58 +0200

    fix; off-by-one date query and current file copy from non-existent path

--- generate_daily.py
+++ generate_daily.py
@@ -17,12 +17,12 @@ def main():
     now = datetime.now()
     last_midnight = datetime(year=now.year, month=now.month, day=now.day)
     db = MalshareDB()
-    all_path = md5_path = sha1_path = sha256_path = None
+    latest_all = latest_md5 = latest_sha1 = latest_sha256 = None
     try:
         current_date = db.first_date()
         while current_date < last_midnight:
             date_string = current_date.strftime("%Y-%m-%d")
-            current_date += timedelta(days=1)
+            next_date = current_date + timedelta(days=1)
 
             date_path = os.path.join(OUTPUT_DIR, date_string)
             all_file_name = F"malshare_fileList.{date_string}.all.txt"
@@ -35,6 +35,11 @@ def main():
             sha1_path = os.path.join(date_path, sha1_file_name)
             sha256_path = os.path.join(date_path, sha256_file_name)
             if all(os.path.exists(p) for p in [all_path, md5_path, sha1_path, sha256_path]):
+                latest_all = all_path
+                latest_md5 = md5_path
+                latest_sha1 = sha1_path
+                latest_sha256 = sha256_path
+                current_date = next_date
                 continue
 
             all_h = hashlib.sha256()
@@ -43,7 +48,7 @@ def main():
             sha256_h = hashlib.sha256()
 
             all_fp = md5_fp = sha1_fp = sha256_fp = None
-            for row in db.db_added_between(current_date, current_date + timedelta(days=1)):
+            for row in db.db_added_between(current_date, next_date):
                 if all_fp is None:
                     try:
                         os.mkdir(date_path)
@@ -72,14 +77,20 @@ def main():
                     fp.write(F"{md5_h.hexdigest()}  {md5_file_name}\n")
                     fp.write(F"{sha1_h.hexdigest()}  {sha1_file_name}\n")
                     fp.write(F"{sha256_h.hexdigest()}  {sha256_file_name}\n")
+                latest_all = all_path
+                latest_md5 = md5_path
+                latest_sha1 = sha1_path
+                latest_sha256 = sha256_path
+
+            current_date = next_date
 
     finally:
         db.close()
-        if all_path:
-            shutil.copy(all_path, os.path.join(OUTPUT_DIR, "malshare.current.all.txt"))
-            shutil.copy(sha256_path, os.path.join(OUTPUT_DIR, "malshare.current.sha256.txt"))
-            shutil.copy(sha1_path, os.path.join(OUTPUT_DIR, "malshare.current.sha1.txt"))
-            shutil.copy(md5_path, os.path.join(OUTPUT_DIR, "malshare.current.txt"))
+        if latest_all:
+            shutil.copy(latest_all, os.path.join(OUTPUT_DIR, "malshare.current.all.txt"))
+            shutil.copy(latest_sha256, os.path.join(OUTPUT_DIR, "malshare.current.sha256.txt"))
+            shutil.copy(latest_sha1, os.path.join(OUTPUT_DIR, "malshare.current.sha1.txt"))
+            shutil.copy(latest_md5, os.path.join(OUTPUT_DIR, "malshare.current.txt"))
 
 
 if __name__ == '__main__':
